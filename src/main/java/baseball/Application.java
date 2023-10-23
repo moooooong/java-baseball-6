25779cb4984db94e5f65af8a81ee96f62bf571bb
문고은 <>
2023-10-23 16:47:39 +0900

    feat: Application 클래스 구현
    컴퓨터 랜덤 값 생성, 사용자 입력 값 받고 예외 처리, 결과 출력, 재시작, 종료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,41 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO: 프로그램
+        BaseballGame baseballGame = new BaseballGame();
+        BaseballUI baseballUI = new BaseballUI();
+        baseballUI.printStart();
+        //컴퓨터 랜덤 값 생성
+        List<Integer> computer = baseballGame.computer();
+
+        while(true) {
+            baseballUI.printInput();
+            //유저값 입력
+            String playerStr = Console.readLine();
+            //입력값 예외 체크
+            baseballGame.inputChk(playerStr);
+            baseballGame.playGame(playerStr, computer);
+            //결과 출력
+            boolean result = baseballGame.showResult();
+
+            if (result) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                baseballUI.printReStart();
+                String startBtn = Console.readLine();
+                //컴퓨터 랜덤 값 재생성
+                if(startBtn.equals("1")) computer = baseballGame.computer();
+                //프로그램 종료
+                else if (startBtn.equals("2")) {
+                    return;
+                } else  throw new IllegalArgumentException("1 또는 2만 입력 가능합니다.");
+            }
+        }
     }
 }
